b1eded0f10015717504e4a0643eefd5fb65ce169
Marina Gruzdeva <marinag1899@mail.ru>
2024-04-24 16:22:08 +0300

    fix non determinism

--- simulator/src/server/database.rs
+++ simulator/src/server/database.rs
@@ -15,13 +15,19 @@ impl BoincDatabase {
         }
     }
 
-    pub fn get_map_keys_by_predicate<K: Clone, V, F>(hm: &HashMap<K, V>, predicate: F) -> Vec<K>
+    pub fn get_map_keys_by_predicate<K: Clone + Ord, V, F>(
+        hm: &HashMap<K, V>,
+        predicate: F,
+    ) -> Vec<K>
     where
         F: Fn(&V) -> bool,
     {
-        hm.iter()
+        let mut res = hm
+            .iter()
             .filter(|(_, v)| predicate(*v))
             .map(|(k, _)| (*k).clone())
-            .collect::<Vec<_>>()
+            .collect::<Vec<_>>();
+        res.sort();
+        res
     }
 }
